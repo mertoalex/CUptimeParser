e8ae40075bbd34ca13b24c345bd01a26f12a45ea
mertoalex <baytemurmuslum+github@gmail.com>
2023-03-18 23:56:50 +0300

    forgor :skull:

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 pub fn command_uptime_parser() -> String {
     let cmd = String::from_utf8(std::process::Command::new("uptime").output().unwrap().stdout).unwrap();
-    let saat = cmd.split(',').collect::<Vec<&str>>()[0].split_whitespace().collect::<Vec<&str>>()[2];
-    let mut saatc = saat.clone().to_string();
-    saatc.retain(|c| c == ':');
-    let _saatv = saat.clone().split(':').collect::<Vec<&str>>();
-    let mut saatv: Vec<&str> = Vec::with_capacity(3);
-    for v in _saatv {
+    let clock = cmd.split(',').collect::<Vec<&str>>()[0].split_whitespace().collect::<Vec<&str>>()[2];
+    let mut clockc = clock.clone().to_string();
+    clockc.retain(|c| c == ':');
+    let _clockv = clock.clone().split(':').collect::<Vec<&str>>();
+    let mut clockv: Vec<&str> = Vec::with_capacity(3);
+    for v in _clockv {
         if v.clone().chars().next().unwrap() == '0' {
             let v = v.split("").collect::<Vec<&str>>();
-            saatv.push(v[2]);
+            clockv.push(v[2]);
             continue;
-        }   saatv.push(v);
+        }   clockv.push(v);
     }
-    let saat: String;
-    match saatc.as_str().chars().count() {
-        0 => {saat = format!("{} {}", saatv[0], "minute(s)");}
-        1 => {saat = format!("{} {} {} {}", saatv[0], "hour(s)", saatv[1], "minute(s)");}
-        2 => {saat = format!("{} {} {} {} {} {}", saatv[0], "day(s)", saatv[1], "hour(s)", saatv[2], "minute(s)");}
+    let clock: String;
+    match clockc.as_str().chars().count() {
+        0 => {clock = format!("{} {}", clockv[0], "minute(s)");}
+        1 => {clock = format!("{} {} {} {}", clockv[0], "hour(s)", clockv[1], "minute(s)");}
+        2 => {clock = format!("{} {} {} {} {} {}", clockv[0], "day(s)", clockv[1], "hour(s)", clockv[2], "minute(s)");}
         _   => panic!("more than 2 :")
-    }   saat
+    }   clock
 }
 
 #[cfg(test)]
